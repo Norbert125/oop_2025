43780a9a5520948fdc4301ad69a708831109f9ee
Norbert125 <molnarnorbert55@yahoo.com>
2025-03-19 10:23:20 +0200

    update_Lab4

--- labor-04/src/oop/labor04/Main.java
+++ labor-04/src/oop/labor04/Main.java
@@ -2,6 +2,7 @@ package oop.labor04;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Molnar Norbert");
         System.out.println("Hello, Lab_04");
     }
 }
